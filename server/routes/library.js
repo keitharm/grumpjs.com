1deb0f06a7e708410eef0f6c3645a9c0e7871968
Keith Armstrong <keith1015@gmail.com>
2015-10-01 12:24:20 -0700

    Added routing to sync with CLI requests

--- server/routes/library.js
+++ server/routes/library.js
@@ -1,5 +1,6 @@
 var express   = require('express');
 var path      = require('path');
+var config    = require('../config.json');
 var router    = express.Router();
 var Package   = require('../models/Package');
 
@@ -7,24 +8,57 @@ var Package   = require('../models/Package');
 router.get('/', function(req, res, next) {
   var grump = req.params.grump;
   
-  //instead we need to query mongo
   Package.find({},function (err, result) {
     if(err) console.log(err);
-    res.send(result);
+    if (result.length === 0) {
+      res.sendStatus(404);
+    } else {
+      var data = {};
+      data.user = [];
+      data.grumps = result;
+      res.send(data);
+    }
   });
 
 });
 
-// Search for a specific grump in our DB
-router.get('/:grump?', function(req, res, next) {
-  var grump = req.params.grump;
-  
-  //instead we need to query mongo
-  Package.findOne({command:grump}, function (err, result) {
-    if(err) console.log(err);
-    res.send(result);
-  });
+// Search for a specific grump or user in our DB
+router.get('/:search', function(req, res, next) {
+  var search = req.params.search;
+  var data = {};
 
+  // Query for user with name first
+  Package.find({"owner.login": search}, function (err, result) {
+    if(err) console.log(err);
+    data.user = result;
+
+    // Now query for grumps with this name
+    Package.find({command: search}, function (err, result) {
+      if(err) console.log(err);
+      data.grumps = result;
+
+      res.send(data);
+    });
+  });
+});
+
+// Query if a specific user has a grump
+router.get('/:owner/:grump', function(req, res, next) {
+  var owner = req.params.owner;
+  var grump = req.params.grump;
+
+  //instead we need to query mongo
+  Package.find({"owner.login": owner, "command": grump}, function (err, result) {
+    if(err) console.log(err);
+    if (result === null) {
+      res.sendStatus(404);
+    } else {
+      var data = {};
+      data.user = [];
+      data.grumps = result;
+      res.send(data);
+    }
+  });
 });
 
 
